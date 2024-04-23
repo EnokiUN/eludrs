c5ab76e11ef36d5ef4d58105952a1a964c50ebf2
Oliver Wilkes <oliverwilkes2006@icloud.com>
2024-04-23 23:09:25 +0100

    feat(http): add all user routes

--- src/http.rs
+++ src/http.rs
@@ -2,11 +2,12 @@ use crate::{models::HttpResponse, GatewayClient, REST_URL};
 use anyhow::Result;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
-use serde_json::json;
+use serde_json::{json, Value};
 use std::{fmt::Display, time::Duration};
 use todel::{
     ErrorResponse, InstanceInfo, Message, MessageCreate, MessageDisguise,
-    PasswordDeleteCredentials, Session, SessionCreate, SessionCreated,
+    PasswordDeleteCredentials, ResetPassword, Session, SessionCreate, SessionCreated, UpdateUser,
+    UpdateUserProfile, User, UserCreate,
 };
 use tokio::time;
 
@@ -62,6 +63,7 @@ impl HttpClient {
 
     async fn request<T: for<'a> Deserialize<'a>, B: Serialize + Sized>(
         &self,
+        method: &str,
         path: &str,
         body: Option<B>,
     ) -> Result<HttpResponse<T>> {
@@ -113,7 +115,7 @@ impl HttpClient {
             disguise: None,
         };
         match self
-            .request::<Message, MessageCreate>("messages", Some(message))
+            .request::<Message, MessageCreate>("POST", "messages", Some(message))
             .await?
         {
             HttpResponse::Success(data) => Ok(data),
@@ -132,7 +134,7 @@ impl HttpClient {
             disguise: Some(disguise),
         };
         match self
-            .request::<Message, MessageCreate>("messages", Some(message))
+            .request::<Message, MessageCreate>("POST", "messages", Some(message))
             .await?
         {
             HttpResponse::Success(data) => Ok(data),
@@ -158,6 +160,7 @@ impl HttpClient {
     ) -> Result<SessionCreated> {
         match self
             .request::<SessionCreated, SessionCreate>(
+                "POST",
                 "sessions",
                 Some(SessionCreate {
                     identifier,
@@ -177,6 +180,7 @@ impl HttpClient {
     pub async fn delete_session(&self, session_id: u64, password: String) -> Result<()> {
         match self
             .request::<(), PasswordDeleteCredentials>(
+                "DELETE",
                 &format!("sessions/{}", session_id),
                 Some(PasswordDeleteCredentials { password }),
             )
@@ -189,9 +193,167 @@ impl HttpClient {
 
     /// Get all sessions.
     pub async fn get_sessions(&self) -> Result<Vec<Session>> {
-        match self.request::<Vec<Session>, ()>("sessions", None).await? {
+        match self
+            .request::<Vec<Session>, ()>("GET", "sessions", None)
+            .await?
+        {
             HttpResponse::Success(sessions) => Ok(sessions),
             HttpResponse::Error(err) => Err(anyhow::anyhow!("Could not get sessions: {:?}", err)),
+        }
+    }
+
+    /// Create a user.
+    pub async fn create_user(
+        &self,
+        username: String,
+        email: String,
+        password: String,
+    ) -> Result<User> {
+        match self
+            .request::<User, UserCreate>(
+                "POST",
+                "users",
+                Some(UserCreate {
+                    username,
+                    email,
+                    password,
+                }),
+            )
+            .await?
+        {
+            HttpResponse::Success(user) => Ok(user),
+            HttpResponse::Error(err) => Err(anyhow::anyhow!("Could not create user: {:?}", err)),
+        }
+    }
+
+    /// Delete the current user.
+    pub async fn delete_user(&self, password: String) -> Result<()> {
+        match self
+            .request::<(), PasswordDeleteCredentials>(
+                "DELETE",
+                "users",
+                Some(PasswordDeleteCredentials { password }),
+            )
+            .await?
+        {
+            HttpResponse::Success(_) => Ok(()),
+            HttpResponse::Error(err) => Err(anyhow::anyhow!("Could not delete user: {:?}", err)),
+        }
+    }
+
+    /// Get the current user.
+    pub async fn get_user(&self) -> Result<User> {
+        match self.request::<User, ()>("GET", "users/@me", None).await? {
+            HttpResponse::Success(user) => Ok(user),
+            HttpResponse::Error(err) => Err(anyhow::anyhow!("Could not get user: {:?}", err)),
+        }
+    }
+
+    /// Update the current user.
+    pub async fn update_user(&self, update: UpdateUser) -> Result<User> {
+        match self
+            .request::<User, UpdateUser>("PATCH", "users", Some(update))
+            .await?
+        {
+            HttpResponse::Success(user) => Ok(user),
+            HttpResponse::Error(err) => Err(anyhow::anyhow!("Could not update user: {:?}", err)),
+        }
+    }
+
+    /// Update the current user's profile.
+    pub async fn update_user_profile(&self, update: UpdateUserProfile) -> Result<User> {
+        match self
+            .request::<User, UpdateUserProfile>("PATCH", "users/profile", Some(update))
+            .await?
+        {
+            HttpResponse::Success(user) => Ok(user),
+            HttpResponse::Error(err) => {
+                Err(anyhow::anyhow!("Could not update user profile: {:?}", err))
+            }
+        }
+    }
+
+    /// Get a user by their id.
+    pub async fn get_user_by_id(&self, user_id: u64) -> Result<User> {
+        match self
+            .request::<User, ()>("GET", &format!("users/{}", user_id), None)
+            .await?
+        {
+            HttpResponse::Success(user) => Ok(user),
+            HttpResponse::Error(err) => Err(anyhow::anyhow!("Could not get user: {:?}", err)),
+        }
+    }
+
+    /// Get a user by their username.
+    pub async fn get_user_by_username(&self, username: String) -> Result<User> {
+        match self
+            .request::<User, ()>("GET", &format!("users/{}", username), None)
+            .await?
+        {
+            HttpResponse::Success(user) => Ok(user),
+            HttpResponse::Error(err) => Err(anyhow::anyhow!("Could not get user: {:?}", err)),
+        }
+    }
+
+    /// Verify your email address.
+    pub async fn verify_user(&self, code: String) -> Result<()> {
+        match self
+            .request::<(), u8>("POST", &format!("/users/verify?{code}"), None)
+            .await?
+        {
+            HttpResponse::Success(_) => Ok(()),
+            HttpResponse::Error(err) => Err(anyhow::anyhow!("Could not verify user: {:?}", err)),
+        }
+    }
+
+    /// Resend the verification email.
+    pub async fn resend_verification(&self) -> Result<()> {
+        match self
+            .request::<(), u8>("POST", "/users/resend-verification", None)
+            .await?
+        {
+            HttpResponse::Success(_) => Ok(()),
+            HttpResponse::Error(err) => Err(anyhow::anyhow!(
+                "Could not resend verification email: {:?}",
+                err
+            )),
+        }
+    }
+
+    /// Create password reset code.
+    pub async fn create_password_reset(&self, email: String) -> Result<()> {
+        match self
+            .request::<(), serde_json::Value>(
+                "POST",
+                "/users/reset-password",
+                Some(json!({ "email": email })),
+            )
+            .await?
+        {
+            HttpResponse::Success(_) => Ok(()),
+            HttpResponse::Error(err) => Err(anyhow::anyhow!(
+                "Could not create password reset code: {:?}",
+                err
+            )),
+        }
+    }
+
+    /// Reset your password.
+    pub async fn reset_password(&self, code: u32, email: String, password: String) -> Result<()> {
+        match self
+            .request::<(), ResetPassword>(
+                "POST",
+                "/users/reset-password",
+                Some(ResetPassword {
+                    code,
+                    email,
+                    password,
+                }),
+            )
+            .await?
+        {
+            HttpResponse::Success(_) => Ok(()),
+            HttpResponse::Error(err) => Err(anyhow::anyhow!("Could not reset password: {:?}", err)),
         }
     }
 }
